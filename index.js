07e2d24aa02aa72ba66c89bf2872ef62ad7b5fab
45otten <145984169+45otten@users.noreply.github.com>
2023-11-01 20:24:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ let output = "";
 const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
-    output = eval(output.replace("%", "/100"));
+    if (output.includes('%')) {
+      const parts = output.split(/([+-])/);
+      for (let i = 0; i < parts.length; i++) {
+        if (parts[i].includes('%')) {
+          // Replace '%' with '/100' within the part
+          parts[i] = parts[i].replace(/%/g, '/100');
+        }
+      }
+      output = parts.join('');
+    }
+    output = eval(output);
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
