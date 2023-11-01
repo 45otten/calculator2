91225f4ae1391ed18911ee74fcef0d4010947de0
45otten <145984169+45otten@users.noreply.github.com>
2023-11-01 20:50:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
 
+//Define function to calculate based on button clicked.
 const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
-    output = eval(output.replace("%", "/100*"));
+    //If output has '%', replace with '/100' before evaluating.
+    output = eval(output.replace("%", "/100"));
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
+    //If DEL button is clicked, remove the last character from the output.
     output = output.toString().slice(0, -1);
   } else {
     //If output is empty and button is specialChars then return
@@ -18,10 +21,9 @@ const calculate = (btnValue) => {
   }
   display.value = output;
 };
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
-});
 
+//Add event listener to buttons, call calculate() on click.
 buttons.forEach((button) => {
+  //Button click listener calls calculate() with dataset value as argument.
   button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
