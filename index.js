787e196158ac0cb405a6f55d5932dec22ad10d46
45otten <abdulahad2a8@gmail.com>
2023-11-02 11:18:22 +0500

    added mod function

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
-const specialChars = ["*", "/", "-", "+", "="]; // Remove "%" from specialChars
 let output = "";
 
 const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
-    output = eval(output.replace("%", "/100*"));
+    try {
+      output = eval(output);
+    } catch (error) {
+      output = "Error";
+    }
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
     output = output.toString().slice(0, -1);
-  } 
-//   else if (btnValue === "%") {
-//     const values = output.split("%");
-//     if (values.length === 2) {
-//       const leftOperand = parseFloat(values[0]);
-//       const rightOperand = parseFloat(values[1]);
-//       if (!isNaN(leftOperand) && !isNaN(rightOperand)) {
-//         output = (leftOperand % rightOperand).toString();
-//       }
-//     }
-//   } 
-   else {
-    if (output === "" && specialChars.includes(btnValue)) return;
+  } else if (btnValue === "MOD") {
+    // Check for the "MOD" operation
+    const values = output.split("%");
+    if (values.length === 2) {
+      const leftOperand = parseFloat(values[0]);
+      const rightOperand = parseFloat(values[1]);
+      if (!isNaN(leftOperand) && !isNaN(rightOperand)) {
+        output = (leftOperand % rightOperand).toString();
+      }
+    }
+  } else {
+    if (output === "Error") return;
     output += btnValue;
   }
   display.value = output;
